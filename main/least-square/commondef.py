5f8b649fc3d01343a4e299da3798fa870a6a4e33
dnikiforov1967 <dnikiforov@mail.ru>
2025-10-18 22:16:27 +0300

    refactoring

---
+++ main/least-square/commondef.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas
+
+def leastSquares(x, y, deg=1):
+    a = np.vander(x, deg + 1)
+    pinv_a = np.linalg.pinv(a)
+    w = pinv_a.dot(y)
+    return w
+
+df = pandas.read_csv("/home/dmitry/Data/flow/example-01.csv")
+
+x_ser = df.x.tolist()
+y_ser = df.y.tolist()
--- main/least-square/cubeApprox.py
+++ main/least-square/cubeApprox.py
@@ -1,22 +1,11 @@
-import pandas
 import numpy as np
+import commondef
 import matplotlib.pyplot as plt
 
-df = pandas.read_csv("/home/dmitry/Data/flow/example-01.csv")
+wm = commondef.leastSquares(commondef.x_ser, commondef.y_ser,3)
 
-x_ser = df.x.tolist()
-y_ser = df.y.tolist()
+z_ser = np.polyval(wm, commondef.x_ser)
 
-def leastSquares(x, y, deg=1):
-    a = np.vander(x, deg + 1)
-    pinv_a = np.linalg.pinv(a)
-    w = pinv_a.dot(y)
-    return w
-
-wm = leastSquares(x_ser, y_ser,3)
-
-z_ser = np.polyval(wm, x_ser)
-
-plt.plot(x_ser, y_ser)
-plt.plot(x_ser, z_ser)
+plt.plot(commondef.x_ser, commondef.y_ser)
+plt.plot(commondef.x_ser, z_ser)
 plt.show()
