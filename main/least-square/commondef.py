08da78e982b981f741e74815d8dfbad12777b274
dnikiforov1967 <dnikiforov@mail.ru>
2025-10-19 17:07:28 +0300

    +comment

--- main/least-square/commondef.py
+++ main/least-square/commondef.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas
 
 def leastSquares(x, y, deg=1):
+    # Матрица Вандермонда
     a = np.vander(x, deg + 1)
+    # Матрица Пенроуза
     pinv_a = np.linalg.pinv(a)
     w = pinv_a.dot(y)
     return w
